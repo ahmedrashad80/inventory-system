705b97f0dcbd9177b19336147c0c92cd78eae65c
ahmedrashad80 <ahmedmohamedrashad80@gmail.com>
2025-07-24 07:27:11 +0300

    use api to check token

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-import Cookies from "js-cookie";
+import axios from "axios";
 
 const ProtectedRoute = ({ children }) => {
-  const token = Cookies.get("token");
-  console.log("ProtectedRoute token:", token);
+  const [loading, setLoading] = useState(true);
+  const [authenticated, setAuthenticated] = useState(false);
 
-  if (!token) {
-    return <Navigate to="/login" replace />;
-  }
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}api/user/verify`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setAuthenticated(true);
+      })
+      .catch(() => {
+        setAuthenticated(false);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return null;
+
+  if (!authenticated) return <Navigate to="/login" replace />;
 
   return children;
 };
